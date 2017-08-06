2024ac9333f278a72af39cc9dd79dfdcdac9de77
haesleinhuepf <rhaase@mpi-cbg.de>
2017-08-06 13:56:17 +0200

    WIP: Fixed the TiffWriter. It may not yet be usable in general for any kind of CLImage

--- src/java/clearcl/io/TiffWriter.java
+++ src/java/clearcl/io/TiffWriter.java
@@ -2,16 +2,29 @@ package clearcl.io;
 
 import java.io.File;
 
+import clearcl.ClearCLImage;
 import clearcl.interfaces.ClearCLImageInterface;
+import coremem.buffers.ContiguousBuffer;
 import coremem.enums.NativeTypeEnum;
+import coremem.offheap.OffHeapMemory;
+import coremem.util.Size;
+import loci.common.services.ServiceFactory;
+import loci.formats.FormatTools;
+import loci.formats.IFormatWriter;
+import loci.formats.ImageWriter;
+import loci.formats.MetadataTools;
+import loci.formats.meta.IMetadata;
+import loci.formats.services.OMEXMLService;
 
 /**
  * Image TIFF writer
  * 
- * TODO: not working until someone figures out how to save tiff files, which
- * does not seem to be me ;-)
+ * TODO: Replace the line marked with "Dirty hack" below. Otherwise, it may not work with
+ * all kinds of images. Maybe, the ClearCLImage.writeTo method should also be listed in
+ * ClearCLImageInterface
  *
  * @author royer
+ * @author haesleinhuepf
  */
 public class TiffWriter extends WriterBase implements WriterInterface
 {
@@ -39,35 +52,29 @@ public class TiffWriter extends WriterBase implements WriterInterface
   public boolean write(ClearCLImageInterface pImage,
                        File pFile) throws Throwable
   {
-    return pFile.getParentFile().mkdirs();
+    pFile.getParentFile().mkdirs();
 
-    /*
-    int lWidth = (int) pPhantomRenderer.getWidth();
-    int lHeight = (int) pPhantomRenderer.getHeight();
-    int lDepth = (int) pPhantomRenderer.getDepth();
+
+    int lWidth = (int) pImage.getDimensions()[0];
+    int lHeight = (int) pImage.getDimensions()[1];
+    int lDepth = (int) pImage.getDimensions()[2];
     
     int lPixelType = FormatTools.UINT16;
     long lUINT16BitSizeInBytes = lWidth * lHeight
                                  * FormatTools.getBytesPerPixel(lPixelType);
     long lFloatSizeInBytes = lWidth * lHeight * lDepth * Size.FLOAT;
-    
-    if (mTransferMemory == null
-        || lFloatSizeInBytes != mTransferMemory.getSizeInBytes())
-    {
-      mTransferMemory =
-                      OffHeapMemory.allocateBytes("PhantomTiffWriter",
-                                                  lFloatSizeInBytes);
-    }
-    
-    if (mTransferArray == null
-        || lUINT16BitSizeInBytes != mTransferArray.length
-                                    * Size.SHORT)
-    {
-      mTransferArray =
-                     new byte[Math.toIntExact(lUINT16BitSizeInBytes)];
-    }
-    
-    pPhantomRenderer.copyTo(mTransferMemory, true);
+
+    byte[] mTransferArray = new byte[Math.toIntExact(lUINT16BitSizeInBytes)];
+
+    // Dirty hack:
+    ClearCLImage lImage = (ClearCLImage)pImage;
+
+    OffHeapMemory mTransferMemory =
+            OffHeapMemory.allocateBytes(pImage.getSizeInBytes());
+    lImage.writeTo(mTransferMemory, new long[]
+            { 0, 0, 0 }, new long[]
+            { lWidth, lHeight, lDepth }, true);
+
     
     ServiceFactory factory = new ServiceFactory();
     OMEXMLService service = factory.getInstance(OMEXMLService.class);
@@ -89,18 +96,23 @@ public class TiffWriter extends WriterBase implements WriterInterface
     String lFileName = pFile.getAbsolutePath();
     
     System.out.println("Writing image to '" + lFileName + "'...");
-    IFormatWriter writer = new ImageWriter();
+    loci.formats.out.TiffWriter writer = new loci.formats.out.TiffWriter();
+    writer.setCompression(loci.formats.out.TiffWriter.COMPRESSION_LZW);
     writer.setMetadataRetrieve(meta);
     writer.setId(lFileName);
     
     ContiguousBuffer lBuffer = new ContiguousBuffer(mTransferMemory);
-    
+
+
+    float lScaling = getScaling();
+    float lOffset = getOffset();
+
     for (int z = 0; z < lDepth; z++)
     {
       int i = 0;
-      while (lBuffer.hasRemainingFloat() && i<mTransferArray.length)
+      while (lBuffer.hasRemainingFloat() && i < mTransferArray.length)
       {
-        float lFloatValue = lBuffer.readFloat() * mScaling + mOffset;
+        float lFloatValue = lBuffer.readFloat() * lScaling + lOffset;
         int lIntValue = Math.round(lFloatValue);
         byte lLowByte = (byte) (lIntValue & 0xFF);
         byte lHighByte = (byte) ((lIntValue >> 8) & 0xFF);
@@ -114,8 +126,8 @@ public class TiffWriter extends WriterBase implements WriterInterface
     }
     writer.close();
     
-    System.out.println("Done.");/**/
-
+    System.out.println("Done.");
+    return true;
   }
 
 }
