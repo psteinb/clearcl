20313af6657c6453b5d363bce2a347d2aa65b1a7
Peter Steinbach <steinbach@scionics.de>
2019-01-10 15:30:47 +0100

    added class enums for number of channels

--- src/main/java/clearcl/enums/ImageChannelOrder.java
+++ src/main/java/clearcl/enums/ImageChannelOrder.java
@@ -9,13 +9,20 @@ package clearcl.enums;
 public enum ImageChannelOrder
 {
 
+  //TODO: use SingleChannel, DoubleChannel, TripleChannel, QuadChannel
+  //      in the backend for any working implementation (this is device specific)
+  //      this is useful for testing purposes - especially for SingleChannel
+  SingleChannel(1),
  Intensity(1),
  Luminance(1),
  R(1),
  A(1),
+  DoubleChannel(2),
  RG(2),
  RA(2),
+  TripleChannel(3),
  RGB(3),
+  QuadChannel(4),
  RGBA(4),
  ARGB(4),
  BGRA(4);
